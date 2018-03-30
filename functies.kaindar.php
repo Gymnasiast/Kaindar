b10f13fa51a849de9df446bcb14d1161320e90be
Gymnasiast <m.o.steenbeek@gmail.com>
2018-03-30 22:49:53 +0200

    Schoon functies.kaindar.php op

--- functies.kaindar.php
+++ functies.kaindar.php
@@ -1,38 +1,42 @@
 <?php
 
-function geefMaandnaam($maandnummer)
+const MAANDEN = [
+    '',
+    'Januari',
+    'Februari',
+    'Maart',
+    'April',
+    'Mei',
+    'Juni',
+    'Juli',
+    'Augustus',
+    'September',
+    'Oktober',
+    'November',
+    'December',
+];
+
+function geefMaandnaam(int $maandnummer)
 {
-	$maandnummer=ltrim($maandnummer, '0');
-	if (!is_numeric((int)$maandnummer) || $maandnummer<1 || $maandnummer>12)
+	if ($maandnummer < 1 || $maandnummer > 12)
 	{
-		return null;
+		return MAANDEN[0];
 	}
-	$maanden[1]='Januari';
-	$maanden[2]='Februari';
-	$maanden[3]='Maart';
-	$maanden[4]='April';
-	$maanden[5]='Mei';
-	$maanden[6]='Juni';
-	$maanden[7]='Juli';
-	$maanden[8]='Augustus';
-	$maanden[9]='September';
-	$maanden[10]='Oktober';
-	$maanden[11]='November';
-	$maanden[12]='December';
-	return $maanden[$maandnummer];
+
+	return MAANDEN[$maandnummer];
 }
 
 function naarEuro($bedrag)
 {
-	return '&euro;&nbsp;'.number_format($bedrag, 2, ',', '.');
+	return '&euro;&nbsp;' . number_format($bedrag, 2, ',', '.');
 }
 
-function geefHuidigSaldo($rekening='')
+function geefHuidigSaldo(string $rekening = '')
 {
-	$rekeningstring='';
-	if($rekening)
+	$rekeningstring = '';
+	if ($rekening)
 	{
 		$rekeningstring=" WHERE rekening='$rekening'";
 	}
-	return eenregel('SELECT SUM(bij)-SUM(af) FROM mutaties'.$rekeningstring.';');
+	return eenregel('SELECT SUM(bij)-SUM(af) FROM mutaties' . $rekeningstring.';');
 }
