1499d47b104346a12de194fbc994c2759abd08b8
Gymnasiast <m.o.steenbeek@gmail.com>
2018-04-02 22:15:05 +0200

    Release 2.0.0

--- src/CyndaronInfo.php
+++ src/CyndaronInfo.php
@@ -5,6 +5,6 @@ class CyndaronInfo
 {
     const ENGINE_VERSIE = '5.2.98.0';
     const PRODUCT_CODENAAM = '';
-    const PRODUCT_VERSIE = '2.0 alpha 0';
+    const PRODUCT_VERSIE = '2.0.0';
     const PRODUCT_NAAM = 'Kaindar';
 }
